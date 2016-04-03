ab21b3302dd537a100643e37c6f639f2f185e13e
xiaokai <kexiaokai@gmail.com>
2016-04-04 00:31:25 +0800

    [UPD] use cookie to filter dives

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ var _ = require('underscore')
 var app = express()
 var colors = require('colors')
 var http = require('http')
+var cookieParser = require('cookie-parser');
 
 var app = new express()
 
@@ -13,51 +14,61 @@ app.set('port', process.env.PORT || 1024)
 app.set('view engine', 'ejs')
 app.set('root', path.resolve(__dirname, '../').replace(/\/+$/, ''))
 app.set('views', app.get('root') + '/views')
+app.use(cookieParser('novelcn_yzw secret cookie'));
 
 // index
 app.get('/', function(req, res) {
-    var ua = req.headers['user-agent']
-	var current = {
-		name: '当前设备',
-    	info: {
-    		ua: ua
-    	}
-	}
-	
-    res.render('dashboard.ejs', {
-    	current: JSON.stringify(current)
-    })
+  var ua = req.headers['user-agent']
+  var current = {
+    name: '当前设备',
+    info: {
+      ua: ua
+    }
+  }
+
+  res.render('dashboard.ejs', {
+    current: JSON.stringify(current)
+  })
 })
 
 app.get('/client/:cid', function(req, res) {
-	
+
 })
 
-app.get('/device/:cid', function (req, res){
-	var params = req.params
-	var cid = req.params.cid
-	var ua = req.headers['user-agent']
-	var name = '设备'
+app.get('/device/:cid', function(req, res) {
+  var params = req.params
+  var cid = req.params.cid
+  var ua = req.headers['user-agent']
+  var deviceId = '';
 
-	var curClients = clients[cid]
-	if(!_.isUndefined(curClients)){
-		curClients.index ++
-		name += curClients.index
+  deviceId = req.cookies.deviceId || _uuid();
 
-		clientSocket.emit('client:update' + cid, {
-			name: name,
-			cid: cid,
-			info: {
-				ua: ua
-			}
-		})
-	}
+  var curClients = clients[cid]
 
-	res.render('device.ejs',{
-		name: name,
-		cid: cid,
-		ua: ua
-	})
+
+  if (!_.isUndefined(curClients)) {
+    
+    curClients[deviceId] = 1;
+
+    clientSocket.emit('client:update' + cid, {
+      id: deviceId,
+      name: deviceId,
+      cid: cid,
+      info: {
+        ua: ua
+      }
+    })
+  }
+
+  res.cookie('deviceId', deviceId, {
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  })
+
+  res.render('device.ejs', {
+    id: deviceId,
+    cid: cid,
+    ua: ua
+  })
 })
 
 
@@ -70,19 +81,19 @@ var io = require('socket.io').listen(server)
 var clientSocket = io.of('/client')
 var deviceSocket = io.of('/device')
 
-clientSocket.on('connection', function (socket) {
-	var socketId
-    socket.on('client:init', function (data) {
-        socketId = data.cid
-        clients[socketId] = {
-        	index: 0
-        }
-    })
+clientSocket.on('connection', function(socket) {
+  var socketId
+  socket.on('client:init', function(data) {
+    socketId = data.cid
+    clients[socketId] = {
+      index: 0
+    }
+  })
 
-	socket.on('disconnect', function () {
-        if (socketId) delete clients[socketId]
-    })
-    
+  socket.on('disconnect', function() {
+    if (socketId) delete clients[socketId]
+  })
+
 })
 
 // setInterval(function(){
@@ -94,10 +105,26 @@ clientSocket.on('connection', function (socket) {
 
 
 app.use(express.static(path.join(app.get('root'), 'public')))
+
+
 /**
  *  server and port
  **/
+
 var port = app.get('port')
 server.listen(port, function() {
-    console.log('Server is listen on port', String(port).blue)
+  console.log('Server is listen on port', String(port).blue)
 })
+
+
+
+/**
+ * 生成唯一标识码的方法
+ * @returns {string}
+ */
+function _uuid() {
+  function s4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+}
