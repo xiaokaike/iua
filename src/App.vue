97b1c45e466cb76b457146bdea1c813eb46f8913
xiaokai <kexiaokai@gmail.com>
2016-04-06 01:05:30 +0800

    [UPD] change to vue

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <div id="device-menu">
+      <header>
+        YOUR UA
+      </header>
+      <ul class="list">
+        <li class="item"
+            v-for="it in devices" 
+            v-bind:class="active: sel"
+            v-on:click="onDevices(this)">
+            {{$index+1}}  {{it.name}}
+        </li>
+      </ul>
+    </div>
+    <div id="info">
+      <h2>User Agent String explained :</h2>
+      <div>
+          <textarea>{{info.ua}}</textarea>
+      </div>    
+    </div>
+    
+    <div id="qrcode">
+      <img v-bind:src="qrcode">
+      <p>
+          <input value="{{deviceUrl}}" readonly></input>
+      </p>
+      <p class="qrcode-tips">扫描二维码获取设备信息</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import qr from './qr.js'
+
+export default {
+  data () {
+    return {
+      title: 'dashboard',
+      cid: '',
+      devices: [],
+      info: {
+        ua: ''
+      },
+      qrcode: '',
+      deviceUrl: ''
+    }
+  },
+  methods: {
+    fetch () {
+
+    },
+    onDevices (e) {
+      console.log(e)
+      this._resetTab()
+      e.$data.sel = true
+      this.info = e.$data.info
+    },
+    update (data) {
+      this._resetTab()
+      data.sel = true
+      this.devices.push(data)
+      this.info = data.info
+    },
+    _resetTab () {
+      this.devices.forEach(item => {
+        item.sel = false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
